57f06a8b0fc39a1b9c0f27c95f982a339981fcf5
Oumaima AbouElMawahib <abouelmawahib.oumaima@inemail.ine.inpt.ma>
2021-05-22 23:14:52 +0100

    another attempt to fix things

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,9 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {createProxyMiddleware}  = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(
-    createProxyMiddleware("/api", {
-      target: "http://[::1]:5050",
-      changeOrigin: true,
-      secure: false,
-    })
+    createProxyMiddleware(["/api", , "/usr"], { target: "http://[::1]:5050",
+  changeOrigin: true,
+secure:false})
   );
 };
