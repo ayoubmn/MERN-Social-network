03db5904890a58831a4bee9cfb7e41bd78cb4ecb
Oumaima AbouElMawahib <abouelmawahib.oumaima@inemail.ine.inpt.ma>
2021-05-24 22:52:24 +0100

    petit test

--- client/src/components/home/comments/CommentMenu.js
+++ client/src/components/home/comments/CommentMenu.js
@@ -30,7 +30,6 @@ const CommentMenu = ({post, comment, setOnEdit}) => {
                 <div className="nav-item dropdown">
                     <span className="material-icons" id="moreLink" data-toggle="dropdown">
                         more_vert
-
                     </span>
 
                     <div className="dropdown-menu" aria-labelledby="moreLink">
