e654b36865f07323a88c34c498f27501a16c12c9
Atef <58571715+IMagwaI@users.noreply.github.com>
2021-05-26 11:50:36 +0100

    some UI touches

--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -30,8 +30,8 @@ const Profile = () => {
      {
        auth.user._id === id &&
        <div className="profile_tab">
-         <button className={saveTab ? '' : 'active'} onClick={() => setSaveTab(false)} >Posts</button>
-         <button className={saveTab ? 'active' : ''} onClick={() => setSaveTab(true)} >Saved</button>
+         <button  id="except"  className={saveTab ? '' : 'active'} onClick={() => setSaveTab(false)} >Posts</button>
+         <button  id="except"  className={saveTab ? 'active' : ''} onClick={() => setSaveTab(true)} >Saved</button>
          </div>
      } 
 
