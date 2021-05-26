5da8674adfe6448075b945d3fe74e16457436c8c
Oumaima AbouElMawahib <abouelmawahib.oumaima@inemail.ine.inpt.ma>
2021-05-26 20:04:18 +0100

    fix some minor problems

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,5 +1,5 @@
 //export const BASE_URL = 'http://localhost:3000'
 //export const BASE_URL = 'https://garfield-network.herokuapp.com/'
 const production  = 'https://garfield-network.herokuapp.com';
-const development = 'http://localhost:3000/';
-export const BASE_URL  = (process.env.NODE_ENV=="production" ? production : development);
+const development = 'http://localhost:3000';
+export const BASE_URL  = (process.env.NODE_ENV === "production" ? production : development);
